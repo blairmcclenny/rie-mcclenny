e509f6b5518f5cb84318062172167ab7565ba7bf
Blair McClenny <bmcclenny@ayzenberg.com>
2020-03-06 21:21:00 -0800

    update useCurrentWidth

--- src/hooks/UseCurrentWidth.js
+++ src/hooks/UseCurrentWidth.js
@@ -1,29 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const getWidth = () => {
-  return (
-    window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth
-  )
-};
+  if (window !== undefined) {
+    return (
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth
+    )
+  } else {
+    return null
+  }
+}
 
 export const useCurrentWitdh = () => {
-  let [width, setWidth] = useState(getWidth());
+  let [width, setWidth] = useState(getWidth())
 
   useEffect(() => {
-    let timeoutId = null;
-    const resizeListener = () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => setWidth(getWidth()), 150);
-    };
+    let timeoutId = null
 
-    window.addEventListener('resize', resizeListener);
+    const resizeListener = () => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => setWidth(getWidth()), 150)
+    }
+
+    window.addEventListener('resize', resizeListener)
 
     return () => {
-      window.removeEventListener('resize', resizeListener);
+      window.removeEventListener('resize', resizeListener)
     }
   }, [])
 
-  return width;
+  return width
 }
